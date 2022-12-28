b249bf1b3cdc879c63877e10afb2ca00de951398
ilyas <larhdidok@gmail.com>
2022-12-28 00:08:22 +0000

    close to be finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button, Container, Input, Table, TableBody, TableCell, TableHead } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Button, Container } from '@mui/material';
+import { useState } from 'react';
 import React from 'react';
 
 const canGo = (row)=>{
@@ -58,7 +57,6 @@ const changeOtherRows = (arr,newPivotRowValues,pivotRow,pivotCol)=>{// newPivotR
 }
 const calcItteraction = (numbOfExes, arr)=>{
   //the array will be like arr = [["z","x1","x2","e1","e2","e3","results"],[1,-10,-15,0,0,0,0],[0,15,20,1,0,0,10],[0,5,19,0,1,0,10],...];
-  numbOfExes=3;
   const pivotColumn = getPivotColumn(arr[1],numbOfExes); // index of the column
   const pivotRow = getPivotRow(arr,arr.map(val=>val[pivotColumn]));
   console.log("pivotRow : ",pivotRow);
@@ -72,9 +70,7 @@ const calcItteraction = (numbOfExes, arr)=>{
   return arr;
 }
 
-const calcSymplex = (numberOfExes)=>{
-  let arr = [["z","x1","x2","e1","e2","results"],[1,-2,-3,0,0,0],[0,3,5,1,0,29],[0,2,1,0,1,10]];
-  arr = [["z","x1","x2","x3","e1","e2","results"],[1,-7,-8,-10,0,0,0],[0,2,3,2,1,0,1000],[0,1,1,2,0,1,800]];
+const calcSymplex = (numberOfExes, arr)=>{
   // repeat while (canGo(row))
   while(canGo(arr[1])){
     console.log("//////////////////////////////////////////////////");
@@ -83,10 +79,75 @@ const calcSymplex = (numberOfExes)=>{
   console.log("resultaaaaaaa" ,arr);
 }
 
+const calc =(table)=>{
+  const arr = [];
+  //let arr = [["z","x1","x2","e1","e2","results"],[1,-2,-3,0,0,0],[0,3,5,1,0,29],[0,2,1,0,1,10]];
+  //arr = [["z","x1","x2","x3","e1","e2","results"],[1,-7,-8,-10,0,0,0],[0,2,3,2,1,0,1000],[0,1,1,2,0,1,800]];
+  //arr = [["z","x1","x2","e1","e2","e3","results"],[1,-15,-10,0,0,0,0],[0,1,2,1,0,0,24],[0,2,1,0,1,0,45],[0,1,3,0,0,1,30]];
+  const numberOfExes = table[0].length;
+  const numOfConstraints = table.length-1;
+  const numOfCols = table[0].length+table.length;
+  const numOfRows = table.length;
+  console.log(numOfRows,'------------',table,'------------',numOfCols);
+  for(let i=0;i<=numOfRows;i++){
+    arr[i]=[];
+    for(let j=0;j<=numOfCols;j++){
+      if(i===0){
+        if(j===0){
+          arr[i][j]='z';
+        }
+        else if(j<=numberOfExes){
+          arr[i][j]='x'+j;
+        }
+        else if(j===numOfCols){
+          arr[i][j]='results';
+        }
+        else{
+          arr[i][j]='e'+(j-numberOfExes);
+        }
+      }
+      else if(i===1){
+        if(j===0){
+          arr[i][j]=1;
+        }
+        else if(j<=numberOfExes){
+          arr[i][j]=-table[0][j-1];
+        }
+        else{
+          arr[i][j]=0;
+        }
+      }
+      else{
+        if(j===0){
+          arr[i][j]=0;
+        }
+        else if(j<=numberOfExes){
+          arr[i][j]=table[i-1][j-1];
+        }
+        else if(j===numOfCols){
+          arr[i][j]=table[i-1][table[i-1].length-1];  
+        }
+        else if(i-1===j-numberOfExes){
+          arr[i][j]=1;  
+        }
+        else{
+          arr[i][j]=0;
+        }
+      }
+      //arr[i][j]=0;
+    }
+  }
+  console.log('cccccccccccccc',arr,'--numberOfExes--',numberOfExes,'--numOfConstraints--',numOfConstraints);
+
+  
+  calcSymplex(numberOfExes,arr);
+}
+
 
 function App() {
-  const [array, setArry] = useState([["z","x1","x2","x3","e1","e2","results"],[1,-7,-8,-10,0,0,0],[0,2,3,2,1,0,1000],[0,1,1,2,0,1,800]]);
-  const [cols, setCols] = useState();
+  //const [array, setArry] = useState([["z","x1","x2","x3","e1","e2","results"],[1,-7,-8,-10,0,0,0],[0,2,3,2,1,0,1000],[0,1,1,2,0,1,800]]);
+  const [variables, setVariables] = useState([0,0,0,0]);
+  const [constraints, setConstraints] = useState([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]);
   const [numberOfConstraints, setNumberOfconstraints] = useState([0,0,0,0]);
   const [numberOfVariables, setNumberOfVariables] = useState([0,0,0,0]);
   
@@ -99,26 +160,53 @@ function App() {
       
   //   }
   // }
-  const setNumber=(value,setter)=>{
+  const setVarNumber=(value)=>{
     let arr = [];
     for(let i=0; i<value; i++){
       arr[i]=0;
+      // if(variables.length<=i){
+      //   setVariables(old=>[...old,0]);
+      // }
     }
-    setter(arr);
+    setVariables(old=>arr);
+    setConstraints([arr]);
+    for(let i=1; i<numberOfConstraints.length; i++){
+      setConstraints(old=>[...old,arr]);
+    }
+    setNumberOfVariables(arr);
   }
-  console.log("-----",numberOfConstraints,"/////",numberOfVariables);
+
+  const setConstNumber=(value)=>{
+    let arr = [];
+    for(let i=0; i<value; i++){
+      arr[i]=0;
+      // if(constraints.length<=i){
+      //   setConstraints(old=>[...old,variables.map(item=>0)]);
+      // }
+    }
+    setConstraints(arr.map(item=>variables.map(i=>0)));
+    setNumberOfconstraints(arr);
+  }
+  
   return (
     <div className="App">
       <Container>  
         <div>
-          <div>Number of constraints: <input type="number" value={numberOfConstraints.length} onChange={(event)=>{setNumber(event.target.value,setNumberOfconstraints)}} min={2} /></div>
-          Number of variables: <input type="number" value={numberOfVariables.length} onChange={(event)=>{setNumber(event.target.value,setNumberOfVariables)}} min={2} />
+          <div>Number of constraints: <input type="number" value={numberOfConstraints.length} onChange={(event)=>{setConstNumber(event.target.value)}} min={2} /></div>
+          Number of variables: <input type="number" value={numberOfVariables.length} onChange={(event)=>{setVarNumber(event.target.value)}} min={2} />
         </div>
         <div>
           <h3>Enter the values of the objective function:</h3>
           F(x) = 
           {numberOfVariables.map((elementInArray, i) => ( 
-              <span key={`h${i}`}><input style={{ width:"2em" }}/> x{i} </span>
+              <span key={`h${i}`}>
+                <input 
+                  style={{ width:"3em" }} 
+                  value={variables[i]!==undefined ? variables[i]:""} 
+                  onChange={(event)=>setVariables(old=>old.length!==0?old.map((item,index)=>index===i?event.target.value:item):[event.target.value])}
+                  />
+                  {` x${i} `}
+                </span>
               ) 
           )}
         </div>
@@ -129,7 +217,15 @@ function App() {
                 <div key={`i${i}`} style={{ marginBottom:"1em" }}>
                 {
                   numberOfVariables.map((elementInArray, j) => ( 
-                    <span key={`j${j}`}><input style={{ width:"2em" }}/> x{j} </span>
+                    <span key={`j${j}`}>
+                      <input 
+                        style={{ width:"3em" }} 
+                        value={constraints[i][j]!==undefined ? constraints[i][j]:""}
+                        onChange={(event)=>setConstraints(old=>old.length!==0?old.map((item,index)=>index===i?old[i].length!==0?old[i].map((item,index)=>index===j?event.target.value:item):[event.target.value]:item):[event.target.value])}
+                      
+                      /> 
+                        {` x${j} `}
+                      </span>
                     ))
                 }
                 <select id="cars" name="cars" style={{ width:"3em", marginRight:"1em" }}>
@@ -139,14 +235,23 @@ function App() {
                   <option value="fiat">{"<="}</option>
                   <option value="audi">{">="}</option>
                 </select>
-                <input style={{ width:"2em" }}/>
+                <input 
+                  style={{ width:"2em" }} 
+                  value={constraints[i][numberOfVariables.length]!==undefined ? constraints[i][constraints[0].length]:""}
+                  onChange={(event)=>setConstraints(old=>{
+                    let myarr = [...old];
+                    myarr[i][numberOfVariables.length] = event.target.value;
+                    return myarr;
+                  })}
+                  
+                />
                 </div>
               )
             }
           )}
         </div>
         <div>
-          <Button fullWidth variant='contained'>=</Button>
+          <Button fullWidth variant='contained' onClick={()=>calc([variables,...constraints])}>=</Button>
         </div>
       </Container>
     </div>
